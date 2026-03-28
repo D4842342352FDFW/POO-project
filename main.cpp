3e14df03913f533b1734d4e4b2c6f6d94b428cef
Iosub Dragos <iosubdragosc@gmail.com>
2026-03-28 16:41:43 +0200

    proiect nou

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include "Material.h"
-#include "UnitateReciclare.h"
-#include "Senzor.h"
-#include "SenzorContaminare.h"
 
 int main(){
-    Material pet("#2445", "Plastic", 0.1, 0.25);
-    pet.afisare();
-    
-    SenzorContaminare s1(0.35);
-    s1.afisareInformatii();
+
     return 0;
 }
